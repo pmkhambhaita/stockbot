171df2eb8d078188ff8ae364374eaf0cd3b1f6a6
pmkhambhaita <pmkhambhaita@outlook.com>
2025-02-24 09:27:50 +0000

    Added a new graph in greater detail and a prototype of a BFS algorithm.
    BFS is a relatively good algorithm, and a good starting point for this project as it is simple and easy to develop.

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 graph = {
     'A': ['B', 'C'],
     'B': ['A', 'D', 'E'],
-    'C': ['A', 'F'],
-    'D': ['B'],
-    'E': ['B', 'F'],
-    'F': ['C', 'E']
+    'C': ['A', 'F', 'G'],
+    'D': ['B', 'H'],
+    'E': ['B', 'I', 'J'],
+    'F': ['C', 'K'],
+    'G': ['C', 'L'],
+    'H': ['D'],
+    'I': ['E'],
+    'J': ['E', 'M'],
+    'K': ['F'],
+    'L': ['G'],
+    'M': ['J']
 }
 
-print(graph)
+def bfs(graph_in, start, end):
+    visited = []
+    queue = [start]
+
+    while queue:
+        node = queue.pop(0)
+        if node == end:
+            visited.append(node)
+            return visited
+        if node not in visited:
+            visited.append(node)
+            queue.extend(graph_in[node])
+
+    return visited
+
+start_node = 'A'
+end_node = 'H'
+print(bfs(graph, start_node, end_node))
